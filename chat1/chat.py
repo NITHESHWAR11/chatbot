c2d9672507f38c204b16bfba3d7f034e5db14d96
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-09 09:36:52 +0530

    Update chat.py

--- chat1/chat.py
+++ chat1/chat.py
@@ -9,7 +9,7 @@ from .nltk_utilits import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('./intents.json', 'r') as json_data:
+with open('https://github.com/NITHESHWAR11/chatbot/blob/main/chat1/intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "./data.pth"
