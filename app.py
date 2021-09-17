8b348baf9e604bbbff0a6c53964ed2a55e7f7e81
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-17 17:28:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from chat1.chat import Bot
+import flask_cors import CORS, cross_origin
 import nltk
 
 nltk.download('punkt')
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app, resources={r"/api/bot/*":{"origins": "*"}})
+app.config['CORS HEADERS'] = 'Content-Type'
 
+@cross_origin()
 class ChatBot(Resource):
     def post(self):
         user_input = request.json['user_input']
