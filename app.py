56b04f5f44ad78e9415f67e2960b9dd34fdbb55f
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-17 11:31:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ api = Api(app)
 class ChatBot(Resource):
     def post(self):
         user_input = request.json['user_input']
-        return jsonify({'bot': str(Bot(user_input))})
+        response = jsonify({'bot': str(Bot(user_input))})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
 api.add_resource(ChatBot, "/api/bot/")
 
