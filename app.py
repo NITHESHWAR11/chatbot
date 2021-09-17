a9f6b99f1cefd1d9368b65ede8e56a9701154474
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-17 17:42:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from chat1.chat import Bot
-import flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import nltk
 
 nltk.download('punkt')
