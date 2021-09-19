2de34a0321ef6276d5754dc0f73a2532aaeceb99
NITHESHWAR11 <67722935+NITHESHWAR11@users.noreply.github.com>
2021-09-19 18:29:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from chat1.chat import Bot
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import nltk
 
 nltk.download('punkt')
 
 app = Flask(__name__)
 api = Api(app)
-# CORS(app, resources={r"/api/*": {"origins": "*"}}, allow_headers=[
-#         "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
-#         supports_credentials=True)
+CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 
-# @cross_origin()
+@cross_origin()
 class ChatBot(Resource):
     def post(self):
         user_input = request.json['user_input']
